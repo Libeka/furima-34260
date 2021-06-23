fa9083f79f5946773be18d919041ac65ddf667b3
Libeka <79636982+Libeka@users.noreply.github.com>
2021-06-23 21:26:51 +0900

    アプリケーションコントローラー変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
 
   def basic_auth
@@ -9,10 +11,9 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  
 
-  private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :introduction, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day])
   end
 end
