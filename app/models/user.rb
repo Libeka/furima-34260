0bdfc6980ca50a01af8c000eb1f1a7ff7c36b2d5
Libeka <79636982+Libeka@users.noreply.github.com>
2021-06-24 15:59:18 +0900

    不要なコメントアウト　削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day, presence: true
 
 
 end
+
