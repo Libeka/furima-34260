eb8fd9d55f78d74389728b794e291694ee05155c
Libeka <79636982+Libeka@users.noreply.github.com>
2021-06-25 20:43:18 +0900

    ストロングパラメーター定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,32 @@
 class ProductsController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!,except: [:index, :show]
 
   def new
-    @products = Product.new
+    @product = Product.new
   end
-  
+
+  def index
+    @products = Product.all.order("created_at DESC")
+  end
+
+  def create
+    @product = Product.new(products_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def products_params
+    params.require(:product).permit(:item_name, :item_info, :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :item_price, :user_id, :image).merge(user_id: current_user.id)
+  end
+
+    
 end
