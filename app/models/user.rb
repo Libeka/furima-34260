102cea760113cdb3ae52d70ad66b98b3e0709bb8
Libeka <79636982+Libeka@users.noreply.github.com>
2021-06-25 15:02:47 +0900

    テストコード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :birth_day, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
   end
 
   with_options presence: true, format: { with: /\A[ァ-ン一]+\z/, message: '全角(カタカナ)を使用してください' } do
-  validates :last_name_kana
+    validates :last_name_kana
     validates :first_name_kana
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,10 +28,30 @@ require 'rails_helper'
           @user.valid?
           expect(@user.errors.full_messages).to include("Nickname can't be blank")
         end
+        it "ユーザー本名は名字が空だと登録できない" do
+          @user.first_name = ''
+          @user.valid?
+          expect(@user.errors.full_messages).to include("First name can't be blank", "First name 全角文字を使用してください")
+        end
+        it "ユーザー本名は名前が空だと登録できない" do
+          @user.last_name = ''
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Last name can't be blank", "Last name 全角文字を使用してください")
+        end
         it "emailが空では登録できない" do
           @user.email = ''
           @user.valid?
           expect(@user.errors.full_messages).to include("Email can't be blank")
+        end
+        it "ユーザー本名は苗字のふりがなが空では登録できない" do
+          @user.first_name_kana = ''
+          @user.valid?
+          expect(@user.errors.full_messages).to include("First name kana can't be blank", "First name kana 全角(カタカナ)を使用してください")
+        end
+        it "ユーザー本名は名前のふりがなが空では登録できない" do
+          @user.last_name_kana = ''
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Last name kana can't be blank", "Last name kana 全角(カタカナ)を使用してください")
         end
         it "重複したemailが存在する場合登録できない" do
           @user.save
@@ -45,7 +65,7 @@ require 'rails_helper'
           @user.valid?
           expect(@user.errors.full_messages).to include("Password can't be blank")
         end
-        
+
         it 'passwordとpassword_confirmationが不一致では登録できないこと' do
           @user.password = '123aaa'
           @user.password_confirmation = '1234aa'
@@ -53,7 +73,14 @@ require 'rails_helper'
           expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
         end
 
-        it 'passwordは半角英語,半角数字のみでは登録できない' do
+        it 'passwordは半角英語のみでは登録できない' do
+          @user.password = 'aaaaaa'
+          @user.password_confirmation = 'aaaaaa'
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Password Passwordには英字と数字の両方を含めて設定してください")
+        end
+
+        it 'passwordは半角数字のみでは登録できない' do
           @user.password = '111111'
           @user.password_confirmation = '111111'
           @user.valid?
