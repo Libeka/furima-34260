12be03dc8099b275048ccb4baa65d5ca4861d39b
Libeka <79636982+Libeka@users.noreply.github.com>
2021-06-26 12:47:08 +0900

    カラム名変更

--- db/migrate/20210625115553_create_products.rb
+++ db/migrate/20210625115553_create_products.rb
@@ -3,12 +3,12 @@ class CreateProducts < ActiveRecord::Migration[6.0]
     create_table :products do |t|
       t.string    :item_name                  , null: false
       t.text      :item_info                  , null: false
-      t.integer   :item_category_id              , null: false
-      t.integer    :item_sales_status_id          , null: false
-      t.integer    :shipping_fee_status_id        , null: false
-      t.integer    :item_prefecture_id            , null: false 
-      t.integer    :item_scheduled_delivery_id    , null: false
-      t.integer    :item_price                 , null: false
+      t.integer   :category_id                , null: false
+      t.integer   :status_id                  , null: false
+      t.integer   :shipping_fee_status_id     , null: false
+      t.integer   :prefecture_id              , null: false 
+      t.integer   :scheduled_delivery_id      , null: false
+      t.integer   :item_price                 , null: false
       t.integer   :user_id                    , foreign_key: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -36,11 +36,11 @@ ActiveRecord::Schema.define(version: 2021_06_25_122916) do
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "item_name", null: false
     t.text "item_info", null: false
-    t.integer "item_category_id", null: false
-    t.integer "item_sales_status_id", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
     t.integer "shipping_fee_status_id", null: false
-    t.integer "item_prefecture_id", null: false
-    t.integer "item_scheduled_delivery_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "scheduled_delivery_id", null: false
     t.integer "item_price", null: false
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
