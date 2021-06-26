ad0162a8ec043b0e3d864ac3eec779e6490d60a3
Libeka <79636982+Libeka@users.noreply.github.com>
2021-06-26 13:10:04 +0900

    jsファイル修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
-require("../products_price")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
