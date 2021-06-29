6a6a434e7b8e17ea506edb96fabd574324cd1dcb
Libeka <79636982+Libeka@users.noreply.github.com>
2021-06-29 16:21:36 +0900

    factriesbot記述　修正

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :product do
-    
+
     item_name{"あ"}
     item_info{"あ"}
-    category_id{"ア"}
-    status_id{"ア"}
-    shipping_fee_status_id{"ア"}
-    prefecture_id{"ア"}
-    scheduled_delivery_id{"ア"}
-    item_price {"ア"}
-    user_id {"ア"}
+    category_id{2}
+    status_id{2}
+    shipping_fee_status_id{2}
+    prefecture_id{2}
+    scheduled_delivery_id{2}
+    item_price {2}
+    user_id {2}
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,54 +9,71 @@ RSpec.describe Product, type: :model do
   describe '商品出品' do
     #context '商品出品がうまくいくとき' do
       it 'ログイン状態のユーザーのみ、商品出品ページへ遷移できること' do
+        expect(@product).to be_valid
       end
   
       it 'ログアウト状態のユーザーは、商品出品ページへ遷移しようとすると、ログインページへ遷移すること' do
+        expect(@product).to be_valid
       end
   
       it '商品画像を1枚つけることが必須であること' do
+        expect(@product).to be_valid
       end
   
       it '商品名が必須であること' do
+        expect(@product).to be_valid
       end
   
       it '商品の説明が必須であること' do
+        expect(@product).to be_valid
       end
   
       it 'カテゴリーの情報が必須であること' do
+        expect(@product).to be_valid
       end
   
       it '商品の状態についての情報が必須であること' do
+        expect(@product).to be_valid
       end
 
       it '配送料の負担についての情報が必須であること' do
+        expect(@product).to be_valid
       end
 
       it '発送元の地域についての情報が必須であること' do
+        expect(@product).to be_valid
       end
 
       it '発送までの日数についての情報が必須であること' do
+        expect(@product).to be_valid
       end
 
       it '販売価格についての情報が必須であること' do
+        expect(@product).to be_valid
       end
 
       it '販売価格は、¥300~¥9,999,999の間のみ保存可能であること' do
+        expect(@product).to be_valid
       end
 
       it '販売価格は半角数字のみ保存可能であること' do
+        expect(@product).to be_valid
       end
 
       it '入力された販売価格によって、販売手数料や販売利益の表示が変わること' do
+        expect(@product).to be_valid
       end
 
       it '必要な情報を適切に入力すると、商品情報がデータベースに保存されること' do
+        expect(@product).to be_valid
       end
 
       it 'エラーハンドリングができていること（入力に問題がある状態で「出品する」ボタンが押された場合、情報は保存されず、出品ページに戻りエラーメッセージが表示されること）' do
+        expect(@product).to be_valid
       end
 
       it 'エラーハンドリングの際、1つのエラーに対して同じエラーメッセージが重複して表示されないこと' do
+        expect(@product).to be_valid
       end
     #end
 
