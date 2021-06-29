10827be604fe0ef3c284cac8a86811f4ddb2b3c1
Libeka <79636982+Libeka@users.noreply.github.com>
2021-06-29 17:19:39 +0900

    テストコード　異常系　記述

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Product, type: :model do
   end
 
   describe '商品出品' do
-    #context '商品出品がうまくいくとき' do
+    context '商品出品がうまくいくとき' do
       it 'ログイン状態のユーザーのみ、商品出品ページへ遷移できること' do
         expect(@product).to be_valid
       end
@@ -20,35 +20,7 @@ RSpec.describe Product, type: :model do
         expect(@product).to be_valid
       end
   
-      it '商品名が必須であること' do
-        expect(@product).to be_valid
-      end
-  
-      it '商品の説明が必須であること' do
-        expect(@product).to be_valid
-      end
-  
       it 'カテゴリーの情報が必須であること' do
-        expect(@product).to be_valid
-      end
-  
-      it '商品の状態についての情報が必須であること' do
-        expect(@product).to be_valid
-      end
-
-      it '配送料の負担についての情報が必須であること' do
-        expect(@product).to be_valid
-      end
-
-      it '発送元の地域についての情報が必須であること' do
-        expect(@product).to be_valid
-      end
-
-      it '発送までの日数についての情報が必須であること' do
-        expect(@product).to be_valid
-      end
-
-      it '販売価格についての情報が必須であること' do
         expect(@product).to be_valid
       end
 
@@ -75,10 +47,59 @@ RSpec.describe Product, type: :model do
       it 'エラーハンドリングの際、1つのエラーに対して同じエラーメッセージが重複して表示されないこと' do
         expect(@product).to be_valid
       end
-    #end
+    end
 
-    #context '商品出品がうまくいかないとき' do
-    #end
+    context '商品出品がうまくいかないとき' do
+
+      it "商品名が空だと登録できない" do
+        @product.item_name = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Item name can't be blank")
+      end
+
+      it "商品の説明が空だと登録できない" do
+        @product.item_info = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Item info can't be blank")
+      end
+
+      it "商品の状態が空だと登録できない" do
+        @product.status_id = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Status is not a number")
+      end
+
+      it "配送料の負担が空だと登録できない" do
+        @product.shipping_fee_status_id = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Shipping fee status is not a number")
+      end
+
+      it "発送元の地域が空だと登録できない" do
+        @product.prefecture_id = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Prefecture is not a number")
+      end
+
+      it "発送までの日数がからだと登録できない" do
+        @product.scheduled_delivery_id = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Scheduled delivery is not a number")
+      end
+
+      it "販売価格が空だと登録できない" do
+        @product.item_price = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Item price can't be blank")
+      end
+
+      it "販売価格が空だと登録できない" do
+        @product.item_price = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Item price can't be blank")
+      end
+
+    end
 
   end
 end
