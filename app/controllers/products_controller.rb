dd1b95d558284f9eb744d755bd53630e91e2eccf
Libeka <79636982+Libeka@users.noreply.github.com>
2021-06-30 19:32:17 +0900

    テーブル　モデルファイル修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,33 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!,except: [:index, :show]
+  
+  def new
+  @product = Product.new
+  end
+  
   def index
+  @products = Product.all.order("created_at DESC")
+  end
+  
+  def create
+  @product = Product.new(products_params)
+  if @product.save
+  redirect_to root_path
+  else
+  render :new
+  end
+  end
+  
+  #def show
+  #@product = Product.find(params[:id])
+  #end
+  # ↑ 詳細機能の時に定義する
+  
+  private
+  
+  def products_params
+  params.require(:product).permit(:item_name, :item_info, :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :item_price, :user_id, :image).merge(user_id: current_user.id)
   end
   
 end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "products#index"
-  #resources :products, only: [:new, :create, :show, :index ]
+  resources :products, only: [:new, :create,:index ]
+
+  # ↑ 詳細機能の時resourcesにshowアクション記述する
 
 end
