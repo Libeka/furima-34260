57a4d7e84493df64fc98fe3cc117cfab9eeab0e1
Libeka <79636982+Libeka@users.noreply.github.com>
2021-07-01 13:25:10 +0900

    productsコントローラー　修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,32 +1,34 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!,except: [:index, :show]
-  
+
   def new
-  @product = Product.new
+    @product = Product.new
   end
-  
+
   def index
-  @products = Product.all.order("created_at DESC")
+    @products = Product.all.order("created_at DESC")
   end
-  
+
   def create
-  @product = Product.new(products_params)
-  if @product.save
-  redirect_to root_path
-  else
-  render :new
+    @product = Product.new(products_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  end
-  
+
   #def show
-  #@product = Product.find(params[:id])
+    #@product = Product.find(params[:id])
   #end
-  # ↑ 詳細機能の時に定義する
-  
+
+  # ↑ 詳細表示の時記述
+
   private
-  
+
   def products_params
-  params.require(:product).permit(:item_name, :item_info, :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :item_price, :user_id, :image).merge(user_id: current_user.id)
+    params.require(:product).permit(:item_name, :item_info, :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :item_price, :user_id, :image).merge(user_id: current_user.id)
   end
-  
+
+    
 end
