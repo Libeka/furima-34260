1e9344c4084c944626dfec51cac545bd913e4bdf
Libeka <79636982+Libeka@users.noreply.github.com>
2021-07-02 15:47:22 +0900

    コントローラー　editアクション定義

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,10 +18,19 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
   def destroy
     #@product = Product.find(params[:id])
     #@product.destroy
   end
+
+
+
+
+
 
   def show
     @product = Product.find(params[:id])
