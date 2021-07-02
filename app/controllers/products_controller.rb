8f2c90f94ac33b6de9fe9822ae4e70ca6cb55707
Libeka <79636982+Libeka@users.noreply.github.com>
2021-07-02 19:56:41 +0900

    不要なコメントアウト削除

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!,except: [:index, :show]
   before_action :set_product, only: [:edit, :show, :update]
-  #before_action :redirect_to root_path unless current_user.id == @product.user_id ,only:[:edit, :update]
   before_action :redirects_to,only:[:edit, :update]
 
   def new
