4a9f837a055ffc16941eb8be80c483e5e797e658
Libeka <79636982+Libeka@users.noreply.github.com>
2021-07-03 11:50:02 +0900

    バリデーションを設定

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,12 +1,8 @@
 class Card < ApplicationRecord
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :house_number, :building_name, :price, :user_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :item_price, :user_id
 
-  with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is invalid'}
-    validates :user_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
-end
+  validates :price, presence: true
+  belongs_to :user,optional: true
+
 end
