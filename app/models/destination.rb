6eadd09bdb2c7fbac353b35e9f605e20ec65fcfd
Libeka <79636982+Libeka@users.noreply.github.com>
2021-07-06 20:51:00 +0900

    購入履歴、住所モデル　修正

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,12 +1,5 @@
 class Destination < ApplicationRecord
-  
 
   belongs_to :purchase_record
 
-  def save
-    Destination.create(user_id: user_id, buy_item_id: buy_item_id)
-    #Destination.create(content: params[:content])
-    #destination = Destination.create(user_id: user_id, buy_item_id: buy_item_id)
-    #Address.create(postal_code: postal_code, shipping_orig_id: shipping_orig_id, city: city, address_other: address_other, building_name: building_name,telephone_num: telephone_num, sell_item_id: sell_item.id )
-  end
 end
--- app/models/purchase_record.rb
+++ app/models/purchase_record.rb
@@ -5,9 +5,4 @@ class PurchaseRecord < ApplicationRecord
   belongs_to :product
   has_one :destination
 
-
-  def save
-    sell_item = SellItem.create(user_id: user_id, product: product )
-    Address.create(postal_code: postal_code, shipping_orig_id: shipping_orig_id, city: city, address_other: address_other, building_name: building_name,telephone_num: telephone_num, sell_item_id: sell_item.id )
-  end
 end
