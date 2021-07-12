85d477914c2b4c44f028d82221baa53a2fa926fa
Libeka <79636982+Libeka@users.noreply.github.com>
2021-07-12 22:29:36 +0900

    リファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,13 +12,7 @@ class OrdersController < ApplicationController
   def create
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
-      
-      Payjp.api_key = "sk_test_da7b6aa818a27292b7d2016a"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-      Payjp::Charge.create(
-        amount: @product.item_price,  # 商品の値段
-        card: order_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
+      pay_item
       @order_form.save
       return redirect_to root_path
     else
@@ -40,6 +34,14 @@ class OrdersController < ApplicationController
 
   def redirects_to
     redirect_to root_path unless current_user.id == @order_form.user_id
+  end
+
+  def pay_item
+    Payjp::Charge.create(
+      amount: @product.item_price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
   
   end
